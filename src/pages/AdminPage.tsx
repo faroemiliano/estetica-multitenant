b950de159f815e13c1492e52234977e135ec1853
faroemiliano <faroemiliano@gmail.com>
2026-07-14 17:29:54 -0300

    commit para probar

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -178,13 +178,13 @@ function AdminPage() {
                 <a
                   className="text-sm font-semibold text-pink-600"
                   href={`https://wa.me/${c.telefono}?text=${encodeURIComponent(
-                    `¡Feliz cumpleaños, ${c.nombre_completo}! 🎉🎂
+                    `¡Feliz cumpleaños, ${c.nombre_completo}! 
 
 Todo el equipo de Aura Estética te desea un día lleno de alegría, bienestar y momentos especiales.
 
-Gracias por confiar en nosotros. Esperamos verte pronto para seguir acompañándote en tu cuidado y belleza. 💖
+Gracias por confiar en nosotros. Esperamos verte pronto para seguir acompañándote en tu cuidado y belleza. 
 
-¡Que tengas un excelente cumpleaños! 🌸`,
+¡Que tengas un excelente cumpleaños! `,
                   )}`}
                   target="_blank"
                   rel="noreferrer"
